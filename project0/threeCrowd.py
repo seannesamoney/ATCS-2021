a9db9148567357213387dfc52f090dfb361e1cc8
Sean Nesamoney <sean.nesamoney@gmail.com>
2021-10-08 14:20:16 -0700

    If Statement Exercises

---
+++ project0/threeCrowd.py
@@ -0,0 +1,9 @@
+crowdList = ["Sean", "Salma", "Nikhil", "Tommy"]
+if len(crowdList) > 3 :
+        print("The room is crowded")
+
+crowdList.remove("Salma")
+crowdList.remove("Tommy")
+
+if len(crowdList) > 5 :
+        print("The room is crowded")
