c7c2db6db5b7cfe78f5ebb689f40796720b61508
Sean Nesamoney <sean.nesamoney@gmail.com>
2022-05-05 14:33:21 -0700

    Added user input and makes prediction. No errors

--- final_project/ml_hurricanes.py
+++ final_project/ml_hurricanes.py
@@ -1,17 +1,19 @@
 '''
-Program: Hurricanes
+Program: Hurricane Predictor
 Author: Sean Nesamoney
 Course: AT Computer Science
-Date (last modified): April 25, 2022
+Date (last modified): May 2, 2022
 '''
 
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+import matplotlib.image as mpimg
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 
 '''Visualize Data'''
-data = pd.read_csv("data/hurricanes2020.csv")
+data = pd.read_csv("hurricanes2020.csv")
 x_1 = data["lat_1"]
 x_2 = data["long_1"]
 y = data["mx_sustained_winds"]
@@ -34,14 +36,26 @@ print("Corr between Longitude and Max. Wind", x_2.corr(y))
 
 plt.tight_layout()
 #plt.show()
+'''3d Graph'''
+# fig = plt.figure()
+# ax = fig.add_subplot(111, projection='3d')
+#
+# ax.set_xlabel("Latitude")
+# ax.set_ylabel("Longitude")
+# ax.set_zlabel("Wind Speed")
+# ax.scatter(x_1[:, 0], x_2[:, 1], y[:, 2])
+# plt.show()
 
 '''Create Multiple Linear Regression Model'''
 #Organize data into correct format
 x = data[["lat_1", "long_1"]].values
-y = data["mx_sustained_winds"].values
+y = y.values
+# print(x)
+# print(y)
 
 #Separate data into train/test set
 x_train, x_test, y_train, y_test = train_test_split(x,y, test_size = 0.2)
+print(y_test)
 
 #Create model on training data
 model = LinearRegression().fit(x_train, y_train)
@@ -54,22 +68,31 @@ print("Intercept:", model.intercept_)
 print()
 
 '''Compare to test data'''
-#Get the predicted y values for the x_test value
-predictions = model.predict(x_test)
+# Get the predicted y values for the x_test value
+# predictions = model.predict(x_test)
+
+'''User input'''
+lat = float(input("What is the latitude of your region (in degree coordinates)?\n"))
+long = float(input("What is the longitude of your region (in degree coordinates)?\n"))
+
+x_pred = [[lat, long]]
+
+#Get prediction with user input
+predictions = model.predict(x_pred)
+#print(predictions[0])
+y_pred = predictions[0]
 
 #Compare actual and predicted values
-print("Testing Linear Model with Test Data:")
-for index in range(len(x_test)):
-    #Actual y value
-    actual = y_test[index]
+print("Predicting Wind Speed:")
 
-    #Predicted y value
-    y_predict = predictions[index]
 
-    #Test x values
-    x_precipitation = x_test[index][0]
-    x_winter = x_test[index][1]
-    print("Latitude:", x_precipitation)
-    print("Longitude:", x_winter)
-    print("Predicted Max Wind:", y_predict)
-    print("Actual Max Wind:", actual)
+
+#Test x values
+# x_precipitation = x_test[index][0]
+# x_winter = x_test[index][1]
+x_lat = lat
+x_long = long
+print("Latitude:", lat)
+print("Longitude:", long)
+print("Predicted Max Wind:", y_pred)
+
