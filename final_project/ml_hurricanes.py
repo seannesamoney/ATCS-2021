fd7767ef9f7ec383502e7281579a87102c990f2f
Sean Nesamoney <sean.nesamoney@gmail.com>
2022-05-10 23:34:07 -0700

    Final Version. Contains user input, ML model, and data science component

--- final_project/ml_hurricanes.py
+++ final_project/ml_hurricanes.py
@@ -1,115 +1,125 @@
 '''
-Program: Hurricane Predictor
+Program: Hurricane Wind Speed Predictor
 Author: Sean Nesamoney
 Course: AT Computer Science
-Date (last modified): May 2, 2022
+Date (last modified): May 10, 2022
 '''
 import csv
-
 import pandas as pd
 import matplotlib.pyplot as plt
+import self as self
 from sklearn.cluster import KMeans
 import matplotlib.image as mpimg
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+from mpl_toolkits.mplot3d import Axes3D
 
-
-'''Visualize Data'''
+#Read in CSV files
 data = pd.read_csv("hurricanes2020.csv")
 states_data = pd.read_csv("us_states_data.csv")
+#Assign variables to CSV columns
 x_1 = data["lat_1"]
 x_2 = data["long_1"]
 y = data["mx_sustained_winds"]
 y = pd.Series([int(char.split()[0]) for char in y])
-#print(temp)Border Security and Immigration Enforcement Improvements
-# for i in y:
-#     print(i.split())
 
-#CREDITS: https://www.protechtraining.com/blog/post/python-for-beginners-reading-manipulating-csv-files-737
-f = open("us_states_data.csv")
-csv_f = csv.reader(f)
+#Gets U.S state from user input
+def getState():
+    state = input("Enter your state:\n")
+    return state
 
-#MAKE THIS A FUNCTION - SEARCHING FOR ITEM
-state = input("Enter your state:\n")
-lat_in = states_data.loc[states_data["state"] == state]['latitude']
-long_in = states_data.loc[states_data["state"] == state]['longitude']
-# print(lat_in.values)
-# print(lat_in.values[0])
-# print(long_in.values)
+user_state = getState()
 
+#Finds corresponding latitude from given state
+def getLat(data, state):
+    #Credits: https://www.protechtraining.com/blog/post/python-for-beginners-reading-manipulating-csv-files-737
+    f = open("us_states_data.csv")
+    csv_f = csv.reader(f)
 
-fig, graph = plt.subplots(2)
-graph[0].scatter(x_1, y)
-graph[0].set_xlabel("lat_1")
-graph[0].set_ylabel("mx_sustained_winds")
+    lat_in = data.loc[data["state"] == state]['latitude']
+    return lat_in
 
-graph[1].scatter(x_2,y)
-graph[1].set_xlabel("long_1")
-graph[1].set_ylabel("mx_sustained_winds")
+lat = getLat(states_data, user_state)
 
-print("Corr between Latitude and Max. Wind", x_1.corr(y))
-print("Corr between Longitude and Max. Wind", x_2.corr(y))
+#Finds corresponding longitude from given state
+def getLong(data, state):
+    # CREDITS: https://www.protechtraining.com/blog/post/python-for-beginners-reading-manipulating-csv-files-737
+    f = open("us_states_data.csv")
+    csv_f = csv.reader(f)
 
-plt.tight_layout()
-#plt.show()
-'''3d Graph'''
-# fig = plt.figure()
-# ax = fig.add_subplot(111, projection='3d')
-#
-# ax.set_xlabel("Latitude")
-# ax.set_ylabel("Longitude")
-# ax.set_zlabel("Wind Speed")
-# ax.scatter(x_1[:, 0], x_2[:, 1], y[:, 2])
-# plt.show()
+    long_in = data.loc[data["state"] == state]['longitude']
+    return long_in
 
-'''Create Multiple Linear Regression Model'''
-#Organize data into correct format
-x = data[["lat_1", "long_1"]].values
-y = y.values
-# print(x)
-# print(y)
+long = getLong(states_data, user_state)
 
-#Separate data into train/test set
-x_train, x_test, y_train, y_test = train_test_split(x,y, test_size = 0.2)
-#print(y_test)
+'''MACHINE LEARNING'''
+#Uses multilinear regression model to predict maximum wind speed
+def predict(data, y, lat_in, long_in):
+    #Create Multiple Linear Regression Model
+    x = data[["lat_1", "long_1"]].values
+    y = y.values
 
-#Create model on training data
-model = LinearRegression().fit(x_train, y_train)
-
-#Print out model information
-print("Model Information:")
-print("Latitude Coef", model.coef_[0])
-print("Longitude Coef", model.coef_[1])
-print("Intercept:", model.intercept_)
-print()
-
-'''Compare to test data'''
-# Get the predicted y values for the x_test value
-# predictions = model.predict(x_test)
-
-# '''User input'''
-# lat = float(input("What is the latitude of your region (in degree coordinates)?\n"))
-# long = float(input("What is the longitude of your region (in degree coordinates)?\n"))
-# x_pred = [[lat, long]]
-
-x_pred = [[lat_in.values[0], long_in.values[0]]]
-
-#Get prediction with user input
-predictions = model.predict(x_pred)
-#print(predictions[0])
-y_pred = predictions[0]
-
-#Compare actual and predicted values
-print("Predicting Wind Speed:")
+    #Separate data into train/test set
+    x_train, x_test, y_train, y_test = train_test_split(x,y, test_size = 0.2)
 
 
+    #Create model on training data
+    model = LinearRegression().fit(x_train, y_train)
 
-#Test x values
-# x_precipitation = x_test[index][0]
-# x_winter = x_test[index][1]
-x_lat = lat_in
-x_long = long_in
-print("Latitude:", lat_in.values[0])
-print("Longitude:", long_in.values[0])
-print("Predicted Max Wind:", y_pred)
+    #Print out model information
+    print("Model Information:")
+    print("Latitude Coef", model.coef_[0])
+    print("Longitude Coef", model.coef_[1])
+    print("Intercept:", model.intercept_)
+    print()
 
+    '''Compare to test data'''
+
+    x_pred = [[lat_in.values[0], long_in.values[0]]]
+
+    #Get prediction with user input
+    predictions = model.predict(x_pred)
+    y_pred = predictions[0]
+
+    #Compare actual and predicted values
+    print("Predicting Wind Speed:")
+
+    x_lat = lat_in
+    x_long = long_in
+    print("Latitude:", lat_in.values[0])
+    print("Longitude:", long_in.values[0])
+    print("Predicted Max Wind:", y_pred)
+    return y_pred
+
+prediction = predict(data, y, lat, long)
+
+'''DATA SCIENCE'''
+'''Adds predicted maximum wind for a given state to a CSV'''
+def addData(state, wind_speed):
+    #Credits: https://www.delftstack.com/howto/python/python-append-to-csv/
+    data = [state, wind_speed]
+    # Open the file in the write mode
+    with open('states_and_winds.csv', 'a', encoding='UTF8') as f:
+        writer = csv.writer(f)
+
+        # write the data
+        writer.writerow(data)
+
+        f.close()
+
+addData(user_state, prediction)
+
+def plot():
+    winds = pd.read_csv("states_and_winds.csv")
+    state = winds["state"]
+    wind_speed = winds["max_wind_speed"]
+    #Plotting the bar graph
+    plt.bar(state, wind_speed, color='g')
+    plt.title("States and Predicted Wind Speeds")
+    plt.xlabel("U.S State")
+    plt.ylabel("Maximum Wind Speed")
+
+    # Show the plot
+    plt.show()
+
+plot()
